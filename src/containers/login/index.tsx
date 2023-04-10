dfe8fffac2a8bb7d86d051d8488ba7378e709a33
RanielliMontagna <raniellimontagna@hotmail.com>
2023-04-10 11:06:14 -0300

    feat: :sparkles: implementa template para tela de login

--- src/containers/login/index.tsx
+++ src/containers/login/index.tsx
@@ -1,55 +1,37 @@
-import { Button, Grid, Text, Title } from '@mantine/core'
-import { LeftSide, LoginContainer, RightSide, TermosContainer } from './styles'
+import { Grid, Text, Title } from '@mantine/core'
+import { LoginScreen } from '@quantun/login-screen'
 
 import Illustration from 'assets/login/illustration.svg'
-import { IconBrandGoogle } from '@tabler/icons-react'
-import { Link } from 'react-router-dom'
 import { useAuthStore } from 'store/auth/auth'
 
 export function Login() {
   const { login } = useAuthStore()
 
   return (
-    <LoginContainer>
-      <LeftSide>
-        {/* TODO implementar logo */}
-        <Title>Prime Score</Title>
-        <Grid>
-          <img src={Illustration} />
-        </Grid>
-        <Grid style={{ gap: 8, display: 'flex', flexDirection: 'column' }}>
-          <Title order={3}>
-            Transforme sua experiência de gerenciamento de torneios.
-          </Title>
-          <Text size="md" color="gray.6">
-            Junte-se a nós agora e leve seus torneios para o próximo nível.
-          </Text>
-        </Grid>
-      </LeftSide>
-      <RightSide>
-        <div></div>
-        <div>
-          <div>
-            <Title order={3}>Bem vindo ao Prime Score!</Title>
-            <Text size="sm" color="gray.6">
-              Você está a um passo de começar a gerenciar seus torneios de forma
-              mais eficiente.
-            </Text>
-          </div>
-          <div>
-            <Button leftIcon={<IconBrandGoogle size="18" />} onClick={login}>
-              <Text size="sm">Entrar com o Google</Text>
-            </Button>
-          </div>
-        </div>
-        <TermosContainer>
+    <LoginScreen
+      beforeLoginContent={
+        <>
+          <Title order={3}>Bem vindo ao Prime Score!</Title>
           <Text size="sm" color="gray.6">
-            Ao entrar, você concorda com os nossos
-            <br /> <Link to="/termos">Termos de Serviço</Link> e{' '}
-            <Link to="/privacidade">Política de Privacidade</Link>.
+            Você está a um passo de começar a gerenciar seus torneios de forma mais eficiente.
           </Text>
-        </TermosContainer>
-      </RightSide>
-    </LoginContainer>
+        </>
+      }
+      welcomeContent={
+        <>
+          <Title>Prime Score</Title>
+          <Grid>
+            <img src={Illustration} />
+          </Grid>
+          <Grid style={{ gap: 8, display: 'flex', flexDirection: 'column' }}>
+            <Title order={3}>Transforme sua experiência de gerenciamento de torneios.</Title>
+            <Text size="md" color="gray.6">
+              Junte-se a nós agora e leve seus torneios para o próximo nível.
+            </Text>
+          </Grid>
+        </>
+      }
+      onLogin={login}
+    />
   )
 }
