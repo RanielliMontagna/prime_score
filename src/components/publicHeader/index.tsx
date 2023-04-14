1ce0d4366831d5ff49f7d8f402bfe9690071e7f8
RanielliMontagna <raniellimontagna@hotmail.com>
2023-04-14 08:03:47 -0300

    refactor: implementa appbar vindo do quantun core para maior componentização

--- src/components/publicHeader/index.tsx
+++ src/components/publicHeader/index.tsx
@@ -1,108 +1,65 @@
-import {
-  Header as MantineHeader,
-  Group,
-  Button,
-  Divider,
-  Box,
-  Burger,
-  Drawer,
-  ScrollArea,
-  rem,
-  Title,
-} from '@mantine/core'
-import { useDisclosure } from '@mantine/hooks'
-import { IconBrandGoogle } from '@tabler/icons-react'
 import { Link } from 'react-router-dom'
+
+import { AppBar, useAppBarStyles } from '@quantun/core'
+import { Group, Button, Divider, Box, Title } from '@mantine/core'
+import { IconBrandGoogle } from '@tabler/icons-react'
 
 import { useStyles } from './styles'
 import { useAuthStore } from 'store/auth/auth'
 
 export function PublicHeader() {
-  const [drawerOpened, { toggle, close }] = useDisclosure(false)
-  const { classes, theme } = useStyles()
+  const { theme } = useStyles()
+  const { classes } = useAppBarStyles()
   const { login } = useAuthStore()
 
   return (
     <Box>
-      <MantineHeader height={60} px="md">
-        <Group position="apart" sx={{ height: '100%' }}>
-          <Title order={2}>PS</Title>
-
-          <Group
-            sx={{ height: '100%' }}
-            spacing={0}
-            className={classes.hiddenMobile}
-          >
+      <AppBar
+        logo={<Title order={2}>PS</Title>}
+        customSpace={{
+          center: (
+            <Group sx={{ height: '100%' }} spacing={0} className={classes.hiddenMobile}>
+              <Link to="/" className={classes.link}>
+                Home
+              </Link>
+              <Link to="/termos" className={classes.link}>
+                Termos de Serviço
+              </Link>
+              <Link to="/privacidade" className={classes.link}>
+                Política de Privacidade
+              </Link>
+            </Group>
+          ),
+          right: (
+            <Group className={classes.hiddenMobile}>
+              <Button variant="gradient" leftIcon={<IconBrandGoogle size={18} />} onClick={login}>
+                Entrar com o Google
+              </Button>
+            </Group>
+          ),
+        }}
+        itemsDrawer={
+          <>
             <Link to="/" className={classes.link}>
               Home
             </Link>
-            <Link to="/termos" className={classes.link}>
+            <Link to="/termos" className={classes.link} onClick={close}>
               Termos de Serviço
             </Link>
-            <Link to="/privacidade" className={classes.link}>
+            <Link to="/privacidade" className={classes.link} onClick={close}>
               Política de Privacidade
             </Link>
-          </Group>
 
-          <Group className={classes.hiddenMobile}>
-            <Button
-              variant="gradient"
-              leftIcon={<IconBrandGoogle size={18} />}
-              onClick={login}
-            >
-              Entrar com o Google
-            </Button>
-          </Group>
+            <Divider my="sm" color={theme.colorScheme === 'dark' ? 'dark.5' : 'gray.1'} />
 
-          <Burger
-            opened={drawerOpened}
-            onClick={toggle}
-            className={classes.hiddenDesktop}
-          />
-        </Group>
-      </MantineHeader>
-
-      <Drawer
-        opened={drawerOpened}
-        onClose={close}
-        size="100%"
-        padding="md"
-        title="Navegação"
-        className={classes.hiddenDesktop}
-        zIndex={10}
-      >
-        <ScrollArea h={`calc(100vh - ${rem(60)})`} mx="-md">
-          <Divider
-            my="sm"
-            color={theme.colorScheme === 'dark' ? 'dark.5' : 'gray.1'}
-          />
-
-          <Link to="/" className={classes.link}>
-            Home
-          </Link>
-          <Link to="/termos" className={classes.link} onClick={close}>
-            Termos de Serviço
-          </Link>
-          <Link to="/privacidade" className={classes.link} onClick={close}>
-            Política de Privacidade
-          </Link>
-
-          <Divider
-            my="sm"
-            color={theme.colorScheme === 'dark' ? 'dark.5' : 'gray.1'}
-          />
-
-          <Group position="center" grow pb="xl" px="md">
-            <Button
-              variant="gradient"
-              leftIcon={<IconBrandGoogle size={18} />}
-              onClick={login}
-            >
-              Entrar com o Google
-            </Button>
-          </Group>
-        </ScrollArea>
-      </Drawer>
+            <Group position="center" grow pb="xl" px="md">
+              <Button variant="gradient" leftIcon={<IconBrandGoogle size={18} />} onClick={login}>
+                Entrar com o Google
+              </Button>
+            </Group>
+          </>
+        }
+      />
     </Box>
   )
 }
