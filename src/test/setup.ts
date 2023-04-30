6c6386693476870e85491c7f0f4dd7134ad8a3a1
RanielliMontagna <raniellimontagna@hotmail.com>
2023-04-30 18:23:05 -0300

    refactor(tests): :recycle: melhora mocks no setup para testes

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,19 +1,19 @@
-import { expect, afterEach, vi } from 'vitest';
-import { cleanup } from '@testing-library/react';
-import matchers from '@testing-library/jest-dom/matchers';
+import { expect, afterEach, vi } from 'vitest'
+import { cleanup } from '@testing-library/react'
+import matchers from '@testing-library/jest-dom/matchers'
 
 // Extende o expect do jest com os matchers do jest-dom
-expect.extend(matchers);
+expect.extend(matchers)
 
 // Limpa o ambiente após cada teste para evitar problemas
 afterEach(() => {
-  cleanup();
-});
+  cleanup()
+})
 
 // Mock do matchMedia do window para evitar problemas com o @mantined/hooks
 Object.defineProperty(window, 'matchMedia', {
   writable: true,
-  value: vi.fn().mockImplementation(query => ({
+  value: vi.fn().mockImplementation((query) => ({
     matches: false,
     media: query,
     onchange: null,
@@ -26,8 +26,31 @@ Object.defineProperty(window, 'matchMedia', {
 })
 
 //Mock do useNavigate do react-router-dom
-const mockedNavigate = vi.fn();
+const mockedNavigate = vi.fn()
 vi.mock('react-router-dom', () => ({
   ...vi.importActual('react-router-dom'),
   useNavigate: () => mockedNavigate,
-}));
+}))
+
+//Mock do firebase/auth
+vi.mock('firebase/auth', () => ({
+  ...vi.importActual('firebase/auth'),
+  GoogleAuthProvider: class {
+    constructor() {}
+    static credentialFromResult = vi.fn(() => ({
+      accessToken: 'accessToken',
+    }))
+  },
+  signInWithPopup: vi.fn(() =>
+    Promise.resolve({
+      user: {
+        displayName: 'displayName',
+        email: 'email',
+        photoURL: 'photoURL',
+        uid: 'uid',
+      },
+    }),
+  ),
+  signOut: vi.fn(),
+  getAuth: vi.fn(),
+}))
